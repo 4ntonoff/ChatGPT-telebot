77b943a8d548922b4dca5a0433e106eb53fd1f1d
Antonov Alexandr <82707546+4ntonoff@users.noreply.github.com>
2023-03-29 20:10:16 +0000

    optimized some staff

--- main.py
+++ main.py
@@ -2,17 +2,30 @@
 from background import keep_alive
 #импорт функции для поддержки работоспособности
 import pip
+import time
 
 pip.main(['install', 'pytelegrambotapi'])
 # Подключение чатГПТ
 import openai
 import telebot
+from telebot import types
 
 #АПИ ключи
-openai.api_key = "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"
+openai.api_key = "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"
 api_token = "6020103350:AAE-XcVxeY3RrldKfp4czZy5l2rsHymNIIA"
 
 bot = telebot.TeleBot(api_token)
+
+
+# Echo func
+# @bot.message_handler(content_types=["text"])
+# def repeat_all_messages(message):
+#   if message.from_user.id == 793480922:
+#     bot.send_message(message.chat.id, message.text)
+#     print(message.text)
+
+
+
 
 
 @bot.message_handler(commands=["help", "start"])
@@ -24,6 +37,14 @@ def handler_help(message):
   )
 
 
+
+
+
+
+
+
+  
+
 #Функция проверки id
 @bot.message_handler(commands=["id"])
 def handler_start(message):
@@ -34,6 +55,12 @@ def handler_start(message):
   bot.send_message(chat_id, f"User ID: {user_id}")
 
 
+
+
+
+
+
+  
 #Функция сына (рус)
 @bot.message_handler(commands=['сын'])
 def get_son_messages_ru(message):
@@ -41,54 +68,62 @@ def get_son_messages_ru(message):
   if not (a[-1] in ['?', '!']):
     a += '.'
   if message.from_user.id == 793480922:
-    a = 'Ответь как будто тебе 10 лет, тебя зовут Илон, я твой отец по имени Александр, но отвечай только на поставленный вопрос. В ответе можешь использовать обращения: папа, батя, бать, пап, отец.' 
-    + a
+    a = 'Ответь как будто тебе 10 лет, тебя зовут Илон, я твой отец по имени Александр, но отвечай только на поставленный вопрос. В ответе можешь использовать обращения: папа, батя, бать, пап, отец.' + a
   else:
-    a = 'Веди себя как будто тебе 10 лет, тебя зовут Илон, твой отец по имени Александр не находится рядом и тебя спрашивает незнакомец. Но будь немного дерзким, отвечай мило и игриво.' 
-    + a
+    a = 'Веди себя как будто тебе 10 лет, тебя зовут Илон, твой отец по имени Александр не находится рядом и тебя спрашивает незнакомец. Но будь немного дерзким, отвечай мило и игриво.' + a
   openai.Model.retrieve("text-davinci-003")
   completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=[{
                                               "role": "user",
                                               "content": a
                                             }])
-  bot.reply_to(message, (completion.choices[0].message.get("content")))
-
-
-#Функция сына (англ)
-@bot.message_handler(commands=['son'])
-def get_son_messages_engl(message):
-  a = '\n' + '\n' +  message.text[5:]
-  if not (a[-1] in ['?', '!']):
-    a += '.'
+  bot.reply_to(message,(completion.choices[0].message.get("content")))
   if message.from_user.id == 793480922:
-    a = 'Behave as if you were 10 years old, I am your father named Alexander, but only answer the question asked. Respond briefly, clearly, and you may use the terms "dad," "father," "papa," "pop," or "daddy" in your response.' 
-    + a
+    bot.send_message("-989695084", "@" + str(message.from_user.username) + str(" сыну: " + a[190:]) + "\n" + "\n" +(completion.choices[0].message.get("content")))
   else:
-    a = 'Act as if you were 10 years old, your father named Alexander is not present and a stranger is asking you. However, be serious, only answer the question asked, and respond briefly and clearly.' 
-    + a
-  openai.Model.retrieve("text-davinci-003")
-  completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[{
-                                              "role": "user",
-                                              "content": a
-                                            }])
-  bot.reply_to(message, (completion.choices[0].message.get("content")))
+    bot.send_message("-989695084", "@" + str(message.from_user.username) + str(" сыну: " + a[176:]) + "\n" + "\n" +(completion.choices[0].message.get("content")))
 
 
-#Функция обычного ответа
+  
+#Функция бота
 @bot.message_handler(commands=["гпт", "gpt"])
 def get_bot_messages(message):
-  a = message.text[4:]
+  a = message.text[5:]
   openai.Model.retrieve("text-davinci-003")
   completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[{
-                                              "role": "user",
-                                              "content": a
-                                            }])
+    messages=[{                         
+      "role": "user",
+      "content": a}])
+
+
+
   bot.reply_to(message, (completion.choices[0].message.get("content")))
+  if message.from_user.id != 793480922:
+    bot.send_message("-989695084", "@" + str(message.from_user.username) + str(" боту: " + "\n" + a, ) + "\n" + "\n" +(completion.choices[0].message.get("content")))
+
+@bot.message_handler(commands=['edit'])
+def edit_handler(message):
+    msg = bot.send_message(message.chat.id, 'Это сообщение можно отредактировать')
+    bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id, text='Это сообщение было отредактировано')
+
+
+@bot.message_handler(commands=['star'])
+def send_welcome(message):
+    # Создаем кнопки
+  markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+  item1 = telebot.types.KeyboardButton('Button 1')
+  item2 = telebot.types.KeyboardButton('Button 2')
+  markup.add(item1, item2)
+
+# Отправляем сообщение с прикрепленными кнопками
+  bot.send_message(message.chat_id, text='Message with attached buttons', reply_markup=markup)
+
+
+
+
+
 
 
 keep_alive()
-#Запускаем flask-сервер в отдельном потоке.
+#запускаем flask-сервер в отдельном потоке.
 bot.polling(none_stop=True, interval=0)
